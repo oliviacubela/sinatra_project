f36dae92c003973fb6dc869dfcdff7d473d04de4
Olivia Cubela <oliviacubela@gmail.com>
2020-10-27 16:11:20 -0400

    updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base 
   belongs_to :user 
   
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :posts
 
   has_secure_password
+
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :image_url, presence: true
+  validates :email, presence: true, uniqueness: true
 end 
